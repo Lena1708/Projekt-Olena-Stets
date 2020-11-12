1a15128f310c82e7f23ab847cda4a7729dc7f170
Olena Stets <stetsolena7@gmail.com>
2020-11-12 16:22:41 +0100

    рпврпіі

--- js/script.js
+++ js/script.js
@@ -1,8 +1,50 @@
 "use strict";
-function addToDo() {
-    alert("Adding to do")
+
+
+function onButtonClick() {
+    let elem = document.getElementById("elem");
+    if (elem.value!="") {        
+        
+        console.log(elem.value);
+        list.push(elem.value);
+        console.log(list);
+        let json = JSON.stringify(list);
+        localStorage.setItem("mykey", json);
+
+
+        let checkboxWrapper = document.createElement("LI");
+        let checkboxNode = document.createElement("INPUT");
+        checkboxNode.setAttribute("type", "checkbox");
+        let textnode = document.createTextNode(elem.value);
+        checkboxWrapper.appendChild(checkboxNode);
+        checkboxWrapper.appendChild(textnode);
+        document.getElementById("myList").appendChild(checkboxWrapper);
+    }
 }
-onclick="addToDo()"
+let list = []
+
+let temp = localStorage.getItem("mykey");
+
+if (temp != null) {
+    list = JSON.parse(temp);
+    for (let i = 0; i < list.length; i++) {
+        //console.log(list[i])
+
+        let checkboxWrapper = document.createElement("LI");
+        let checkboxNode = document.createElement("INPUT");
+        checkboxNode.setAttribute("type", "checkbox");
+        let textnode = document.createTextNode(list[i]);
+        checkboxWrapper.appendChild(checkboxNode);
+        checkboxWrapper.appendChild(textnode);
+        document.getElementById("myList").appendChild(checkboxWrapper);
+    }
+ }
 
 
-let elem = document.getElementById("elem");
+
+
+
+
+
+
+
